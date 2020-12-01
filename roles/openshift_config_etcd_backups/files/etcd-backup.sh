bab34f9db48dfbf823e357942b01a64ef55217b7
lee <lee.thomas@redhat.com>
2020-12-01 11:38:27 -0600

    upddated etcd backups with a clean up option for backups older than 30 days

--- roles/openshift_config_etcd_backups/files/etcd-backup.sh
+++ roles/openshift_config_etcd_backups/files/etcd-backup.sh
@@ -5,14 +5,12 @@ DATE=$(date +%Y%m%dT%H%M%S)
 /usr/local/bin/etcd-snapshot-backup-disconnected.sh /assets/backup
 
 if [ $? -eq 0 ]; then
-    mkdir /etcd-backup/${DATE}
-    cp -r /assets/backup/*  /etcd-backup/${DATE}/
-    if [ $? -eq 0 ]; then
-    # Making sure we exit clean witout errors only if the cp command is
-    # successful
-        echo 'Copied backup files to PVC mount point.'
-        exit 0
-    fi
+    mkdir /etcd-backup/{{ openshift_cluster_name }}/${DATE}
+    cp -r /assets/backup/*  /etcd-backup/{{ openshift_cluster_name }}/${DATE}/
+    echo 'Deleting backups older than {{ etcd_backup_daysToKeepBackups }} days'
+    find /etcd-backup/{{ openshift_cluster_name }} -type d -mtime +{{ etcd_backup_daysToKeepBackups }} -execdir rm -rf -- '{}' +
+    echo 'Copied backup files to PVC mount point.'
+    exit 0
 fi
 
 echo "Backup attempts failed. Please FIX !!!"
